9579c2ef3d5bacb787d27798b999f7fb32bc7229
stslex <ilya977.077@gmail.com>
2023-07-22 16:16:18 +0300

    test deploy works

--- src/main/kotlin/stslex/com/plugins/Routing.kt
+++ src/main/kotlin/stslex/com/plugins/Routing.kt
@@ -12,6 +12,9 @@ fun Application.configureRouting() {
         get("/api/v1/hello") {
             call.respond(HelloRequest("hello"))
         }
+        get("/api/v1/test-deploy") {
+            call.respond(HelloRequest("test-deploy"))
+        }
         get("/api/v1/hello-naum") {
             call.respond(HelloRequest("hello Naum!!"))
         }
