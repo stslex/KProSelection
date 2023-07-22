8ff53d95b0cf8bf8d740d85c0217bc33c6815e64
stslex <ilya977.077@gmail.com>
2023-07-22 14:11:10 +0300

    add docker deploy

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import io.ktor.plugin.features.*
+import io.ktor.plugin.features.DockerImageRegistry.Companion.dockerHub
+
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
@@ -32,4 +35,28 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
+
+ktor {
+    docker {
+        jreVersion.set(JreVersion.JRE_17)
+        localImageName.set("sample-docker-image")
+        imageTag.set("0.0.1-preview")
+        portMappings.set(
+            listOf(
+                DockerPortMapping(
+                    8080,
+                    8080,
+                    DockerPortMappingProtocol.TCP
+                )
+            )
+        )
+        externalRegistry.set(
+            dockerHub(
+                appName = provider { "kproselection" },
+                username = providers.environmentVariable("DOCKER_HUB_USERNAME"),
+                password = providers.environmentVariable("DOCKER_HUB_PASSWORD")
+            )
+        )
+    }
 }
